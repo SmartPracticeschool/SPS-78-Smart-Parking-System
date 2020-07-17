3ff6d3eaa30560420fdb0928f05a4fb59be6bcd8
lokeshsaravana <66309269+lokeshsaravana@users.noreply.github.com>
2020-07-17 18:52:28 +0530

    Add files via upload

---
+++ amazing_hango_migelo1.ino
@@ -0,0 +1,61 @@
+int trigPin = 7;    // Trigger
+int echoPin = 6 ;// Echo
+int led1 = 3;
+int led2 = 4;
+long duration, cm, inches;
+ 
+void setup() {
+  //Serial Port begin
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+ 
+void loop() {
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  
+  Serial.print(inches);
+  Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  
+  delay(250);
+  {
+   if (cm < 335) {
+    digitalWrite(led1, HIGH);
+}
+    else {
+      digitalWrite(led1, LOW);
+    }
+    if(cm >= 335) {
+    digitalWrite(led2,HIGH);
+  }
+	else {
+      digitalWrite(led2, LOW);
+  }
+      
+  }
+    
+    
+ 
+}
